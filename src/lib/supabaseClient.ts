412ce1f0318f0fb8f740a1f2d4b5d6a69d381266
Riccardo <riccardo.sordini777@gmail.com>
2025-12-16 14:59:15 +0100

    fix url typo com to co

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Usiamo il valore diretto per saltare ogni problema di configurazione
-const supabaseUrl = 'https://obosxrxnkpbaefwyoghu.supabase.com'
+// CORREZIONE: .co invece di .com
+const supabaseUrl = 'https://obosxrxnkpbaefwyoghu.supabase.co'
 const supabaseAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ib3N4cnhua3BiYWVmd3lvZ2h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4MzMyNTEsImV4cCI6MjA4MTQwOTI1MX0.n0hde7ESz1rnuOuTf_QWSe4G_zwKb7sDd6jIMAIxX5Y'
 
 export const supabase = createClient(supabaseUrl, supabaseAnon)
